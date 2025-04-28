5889eb0a8d491d21c819921d7264cdcaa22d5a27
Pravenn18 <insanepraveen2018@gmail.com>
2025-04-28 23:21:54 +0530

    add two jotai atoms

---
+++ data/atom.ts
@@ -0,0 +1,11 @@
+// Define Jotai atoms for global state
+// Updated answer structure to handle multiple selections
+
+import { atom } from "jotai";
+
+export const answersAtom = atom<{
+  [key: number]: {
+    selectedOptions: { option: string; index: number }[];
+  };
+}>({});
+export const currentQuestionIndexAtom = atom<number>(0);
