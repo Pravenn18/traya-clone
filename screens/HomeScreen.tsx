9283e30605937cc44eaf0d0086cfa1fa2300ac10
Pravenn18 <insanepraveen2018@gmail.com>
2025-04-28 23:22:18 +0530

    modified home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,17 +1,35 @@
-import { StyleSheet, Text } from "react-native";
+import TrayaButton from "@/components/TrayaButton";
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, Image, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const router = useRouter();
   return (
     <SafeAreaView style={styles.titleContainer}>
-      <Text className="text-2xl bg-red-500">Home</Text>
+      <Image
+        source={require("../assets/images/traya-home.png")}
+        className="w-full h-96 bg-[#4FAA7F]"
+      />
+      <View className="px-5">
+        <Text className="text-4xl font-semibold pt-10">
+          Know The Root Cause Of Your Hair Loss
+        </Text>
+        <Text className="text-lg font-normal pt-6 pb-7">
+          93% saw results* in 5 months
+        </Text>
+        <TrayaButton
+          title={"Complete The Hair Test"}
+          onPress={() => router.push("/(stack)/hair-loss")}
+        />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
     gap: 8,
