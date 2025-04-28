977da63d1ac1f1b0ff39199fd67ab7b3d402ba15
Pravenn18 <insanepraveen2018@gmail.com>
2025-04-28 23:20:36 +0530

    added hairloss screen

---
+++ app/(stack)/hair-loss.tsx
@@ -0,0 +1,2 @@
+import HairLossScreen from "@/screens/HairLossScreen";
+export default HairLossScreen;
---
+++ screens/HairLossScreen.tsx
@@ -0,0 +1,149 @@
+import Header from "@/components/Header";
+import { questionsData } from "@/constants/Data";
+import React from "react";
+import { View, Text, TouchableOpacity, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAtom } from "jotai";
+import SelectionCard from "@/components/Options";
+import { answersAtom, currentQuestionIndexAtom } from "@/data/atom";
+
+const HairLossScreen = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useAtom(
+    currentQuestionIndexAtom
+  );
+  const [answers, setAnswers] = useAtom(answersAtom);
+  const currentQuestion = questionsData[currentQuestionIndex];
+
+  const handleOptionSelect = (option: string, index: number) => {
+    const currentType = currentQuestion.type || "radio";
+    let newAnswers = { ...answers };
+
+    if (!newAnswers[currentQuestion.id]) {
+      newAnswers[currentQuestion.id] = { selectedOptions: [] };
+    }
+
+    if (currentType === "checkbox") {
+      const currentSelections = newAnswers[currentQuestion.id].selectedOptions;
+      const existingSelectionIndex = currentSelections.findIndex(
+        (selection) => selection.index === index
+      );
+
+      if (existingSelectionIndex >= 0) {
+        currentSelections.splice(existingSelectionIndex, 1);
+      } else {
+        currentSelections.push({ option, index });
+      }
+    } else {
+      newAnswers[currentQuestion.id] = {
+        selectedOptions: [{ option, index }],
+      };
+
+      if (currentQuestionIndex < questionsData.length - 1) {
+        setTimeout(() => {
+          setCurrentQuestionIndex(currentQuestionIndex + 1);
+        }, 300);
+      }
+    }
+
+    setAnswers(newAnswers);
+  };
+
+  const getDefaultSelected = () => {
+    if (answers[currentQuestion.id]) {
+      return answers[currentQuestion.id].selectedOptions.map(
+        (selection) => selection.index
+      );
+    }
+    return [];
+  };
+
+  const renderContinueButton = () => {
+    if (currentQuestion.type === "checkbox") {
+      const hasSelections =
+        answers[currentQuestion.id]?.selectedOptions?.length > 0;
+
+      return (
+        <View className="flex flex-row justify-center w-full mt-4">
+          <TouchableOpacity
+            className={`p-4 rounded-lg w-52 ${
+              hasSelections ? "bg-[#BDD752]" : "bg-gray-300"
+            }`}
+            disabled={!hasSelections}
+            onPress={() => {
+              if (currentQuestionIndex < questionsData.length - 1) {
+                setCurrentQuestionIndex(currentQuestionIndex + 1);
+              }
+            }}
+          >
+            <Text
+              className={`text-center font-medium ${
+                hasSelections ? "text-black" : "text-gray-500"
+              }`}
+            >
+              Next
+            </Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+    return null;
+  };
+
+  const renderLastPage = () => {
+    return (
+      <View>
+        <Image
+          source={require("../assets/images/scalp.png")}
+          className="w-80 h-80"
+        />
+        <Text>Last Screen</Text>
+      </View>
+    );
+  };
+  const determineCardType = () => {
+    return currentQuestion.type || "radio";
+  };
+
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <Header />
+      {currentQuestion.id === 12 ? (
+        renderLastPage()
+      ) : (
+        <ScrollView className="px-5 flex-1">
+          {currentQuestion.title && (
+            <View className="my-6">
+              <Image
+                source={currentQuestion.titleImage}
+                className="w-20 h-20 left-5"
+              />
+              <Text className="font-medium text-4xl mt-4 text-[#8A8B85]">
+                {currentQuestion.title}
+              </Text>
+              <View className="bg-[#F3F3F1] w-20 h-2 mt-6" />
+            </View>
+          )}
+          <Text className="font-normal text-lg mb-6 text-[#8A8B85]">
+            {currentQuestion.question}
+          </Text>
+          <SelectionCard
+            type={determineCardType()}
+            options={currentQuestion.options}
+            optionsImage={currentQuestion.optionsImage}
+            onSelect={handleOptionSelect}
+            defaultSelected={getDefaultSelected()}
+          />
+
+          {renderContinueButton()}
+          <View className="flex-row justify-center items-center my-6">
+            <Text className="text-gray-500">
+              Question {currentQuestionIndex + 1} of {questionsData.length}
+            </Text>
+          </View>
+        </ScrollView>
+      )}
+    </SafeAreaView>
+  );
+};
+
+export default HairLossScreen;
